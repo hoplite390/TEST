38c88b43e88a695b2b4ffc6c3ba40e0f82b5a01c
hoplite390 <43998156+hoplite390@users.noreply.github.com>
2022-01-07 14:25:21 +0100

    Create test.java
    d

---
+++ test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+static void main(String[] args){
+
+	System.out.println("Hello World");
+	Long x = (Long.valueOf((nSortGroup * (nRows + 1L)) + this.sizes[nSortGroup]++));
+}
+}
